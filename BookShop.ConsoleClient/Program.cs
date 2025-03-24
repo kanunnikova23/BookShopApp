c49d05a3a16af0d3d965c08847efed8a7c85f442
Oleksandra <138119282+kanunnikova23@users.noreply.github.com>
2025-03-24 15:53:31 +0000

    add functionality to write into file

--- BookShop.ConsoleClient/Program.cs
+++ BookShop.ConsoleClient/Program.cs
@@ -1,12 +1,23 @@
 ﻿namespace BookShop.ConsoleClient;
 
-abstract class Program
+using System;
+using System.IO;
+
+class Program
 {
     static void Main()
     {
-        Book book1 = new Book("1984", "George Orwell", "Secker & Warburg", 19.99m);
-        Console.WriteLine(book1);
-        book1.Author = "George Orwell";
-    }
+            string filePath = "/Users/oleksandrakanunnikova/RiderProjects/BookShopApp/BookShop.ConsoleClient/console_output";
 
+            Console.WriteLine("Введіть текст для запису у файл:");
+            string input = Console.ReadLine() ?? "";
+
+            using (StreamWriter writer = new StreamWriter(filePath, true))
+            {
+                writer.WriteLine(input);
+            }
+
+            Console.WriteLine($"Текст {input} записано у файл. Дані збережено у {filePath}" );
+
+    }
 }
