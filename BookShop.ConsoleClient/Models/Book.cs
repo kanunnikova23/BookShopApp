b0457aaf662077c3be9e18397f216d49c3f0ebd5
Oleksandra <138119282+kanunnikova23@users.noreply.github.com>
2025-05-03 20:02:05 +0100

    add properties to Book.cs and Genre enum

--- BookShop.ConsoleClient/Models/Book.cs
+++ BookShop.ConsoleClient/Models/Book.cs
@@ -2,10 +2,36 @@ namespace BookShop.ConsoleClient.Models;
 
 public class Book(List<string?> title, List<string> author, decimal price)
 {
+    private int _publicationYear;
     public Book(): this([],[],0){}
     public List<string?> Title { get; set; } = title;
     public List<string> Author { get; set; } = author;
     public decimal Price { get; set; } = price;
+    
+    // Кількість доступних екземплярів (за замовчуванням 0)
+    public int Quantity { get; set; } = 0;
+    
+    // Міжнародний стандартний книжковий номер
+    public string ISBN { get; set; }
+
+    // Рік публікації (наприклад, 2021)
+    public int PublicationYear
+    {
+        get => _publicationYear;
+        set
+        {
+            if (value < 1450 || value > DateTime.Now.Year)
+            {
+                throw new ArgumentOutOfRangeException(nameof(PublicationYear),
+                    "Year must be between 1450 and current year.");
+            }
+
+            _publicationYear = value;
+        }
+    }
+
+    //жанр книжки
+    public Genre Genre { get; set; }
 
 
     public override string ToString()
---
+++ BookShop.ConsoleClient/Models/Genre.cs
@@ -0,0 +1,15 @@
+namespace BookShop.ConsoleClient.Models;
+
+public enum Genre
+{
+    Fantasy,
+    Mystery,
+    Romance,
+    ScienceFiction,
+    NonFiction,
+    Horror,
+    Biography,
+    Historical,
+    Thriller,
+    Other
+}
